e108b43b45d9d3534edd0296e95eb222116ebca6
Kazuki Kyakuno <kyakuno@axell.co.jp>
2022-11-15 10:08:45 +0900

    Update model name

--- image_classification/example_tflite_efficientnet.bat
+++ image_classification/example_tflite_efficientnet.bat
@@ -1,7 +1,7 @@
-python prediction.py -m efficientnetlite --tflite -p float t=1_crop -d imagenet
-python prediction.py -m efficientnetlite --tflite -p legacy t=1_crop -d imagenet
-python prediction.py -m efficientnetlite --tflite -p t=1_crop -d imagenet
+python prediction.py -m efficientnet_lite --tflite -p float t=1_crop -d imagenet
+python prediction.py -m efficientnet_lite --tflite -p legacy t=1_crop -d imagenet
+python prediction.py -m efficientnet_lite --tflite -p t=1_crop -d imagenet
 
-python accuracy.py -m efficientnetlite -d imagenet__float_t_1_crop_tflite
-python accuracy.py -m efficientnetlite -d imagenet__legacy_t_1_crop_tflite
-python accuracy.py -m efficientnetlite -d imagenet__t_1_crop_tflite
+python accuracy.py -m efficientnet_lite -d imagenet__float_t_1_crop_tflite
+python accuracy.py -m efficientnet_lite -d imagenet__legacy_t_1_crop_tflite
+python accuracy.py -m efficientnet_lite -d imagenet__t_1_crop_tflite
